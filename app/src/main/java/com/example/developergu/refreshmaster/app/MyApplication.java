541e88aeffe6f4733ebe048acf239ef1aa991cef
gloriousgu <gloriousgu@126.com>
2018-03-25 18:03:20 +0800

    abstract application

--- app/src/main/java/com/example/developergu/refreshmaster/app/MyApplication.java
+++ app/src/main/java/com/example/developergu/refreshmaster/app/MyApplication.java
@@ -1,45 +1,14 @@
 package com.example.developergu.refreshmaster.app;
 
-import android.app.Application;
-import android.os.StrictMode;
-
 import com.example.developergu.refreshmaster.di.component.AppComponent;
 import com.example.developergu.refreshmaster.di.component.DaggerAppComponent;
-import com.squareup.leakcanary.LeakCanary;
-import com.squareup.leakcanary.RefWatcher;
+import com.gu.mvp.app.BaseApp;
 
-/** Created by developergu on 2018/1/16. */
-public class MyApplication extends Application {
-  private RefWatcher mRefWatcher;
-  private AppComponent mAppComponent;
-
-  public RefWatcher getRefWatcher() {
-    return mRefWatcher;
-  }
+/** Created by developergu on 2018/3/16. */
+public class MyApplication extends BaseApp<AppComponent> {
 
   @Override
-  public void onCreate() {
-    super.onCreate();
-    if (LeakCanary.isInAnalyzerProcess(this)) {
-      // This process is dedicated to LeakCanary for heap analysis.
-      // You should not init your app in this process.
-      return;
-    }
-    enabledStrictMode();
-    mRefWatcher = LeakCanary.install(this);
-    mAppComponent = DaggerAppComponent.builder().build();
-  }
-
-  private static void enabledStrictMode() {
-    StrictMode.setThreadPolicy(
-        new StrictMode.ThreadPolicy.Builder() //
-            .detectAll() //
-            .penaltyLog() //
-            .penaltyDeath() //
-            .build());
-  }
-
-  public AppComponent getAppComponent() {
-    return mAppComponent;
+  public AppComponent createComponent() {
+    return DaggerAppComponent.builder().build();
   }
 }
---
+++ mvplibrary/src/main/java/com/gu/mvp/app/BaseApp.java
@@ -0,0 +1,46 @@
+package com.gu.mvp.app;
+
+import android.app.Application;
+import android.os.StrictMode;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+
+/*<T> dagger single component*/
+public abstract class BaseApp<T> extends Application {
+
+  private RefWatcher mRefWatcher;
+  private T mAppComponent;
+
+  public RefWatcher getRefWatcher() {
+    return mRefWatcher;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      // This process is dedicated to LeakCanary for heap analysis.
+      // You should not init your app in this process.
+      return;
+    }
+    enabledStrictMode();
+    mRefWatcher = LeakCanary.install(this);
+    mAppComponent = createComponent();
+  }
+
+  private static void enabledStrictMode() {
+    StrictMode.setThreadPolicy(
+        new StrictMode.ThreadPolicy.Builder() //
+            .detectAll() //
+            .penaltyLog() //
+            .penaltyDeath() //
+            .build());
+  }
+
+  public T getAppComponent() {
+    return mAppComponent;
+  }
+
+  public abstract T createComponent();
+}
