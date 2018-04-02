ed84c64c2dbe25728b853f0c3e94b85a2688b196
gloriousgu <gloriousgu@126.com>
2018-04-02 17:22:45 +0800

    edit commit

--- app/src/main/java/com/example/developergu/refreshmaster/MainActivity.java
+++ app/src/main/java/com/example/developergu/refreshmaster/MainActivity.java
@@ -11,52 +11,52 @@ import com.gu.mvp.utils.leaks.CleanLeakUtils;
 
 // first commit here!
 public class MainActivity extends AppCompatActivity {
-  ZhiFuBaoIndexView currentFragment;
-  //  static MyApplication mApplication;
+    ZhiFuBaoIndexView currentFragment;
+    //  static MyApplication mApplication;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    currentFragment = getCurrentFragment();
-    if (currentFragment == null) {
-      showNewView();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        currentFragment = getCurrentFragment();
+        if (currentFragment == null) {
+            showNewView();
+        }
+        //    mApplication = (MyApplication) getApplication();
     }
-    //    mApplication = (MyApplication) getApplication();
-  }
 
-  private void showNewView() {
-    currentFragment = ZhiFuBaoIndexView.getInstance();
-    getSupportFragmentManager()
-        .beginTransaction()
-        .replace(R.id.content_view, currentFragment)
-        .commit();
-  }
-
-  private ZhiFuBaoIndexView getCurrentFragment() {
-    return (ZhiFuBaoIndexView) getSupportFragmentManager().findFragmentById(R.id.content_view);
-  }
-
-  @Override
-  public boolean dispatchTouchEvent(MotionEvent ev) {
-    return currentFragment != null
-        && !currentFragment.isCollapseAnimRunning()
-        && super.dispatchTouchEvent(ev);
-  }
-
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    CleanLeakUtils.fixInputMethodManagerLeak(this);
-    checkItem(currentFragment);
-    checkItem(this);
-    currentFragment = null;
-  }
-
-  public void checkItem(Object object) {
-    if (object != null) {
-      Log.e("TAG", "checkItem: " + object);
-      ((MyApplication) getApplication()).getRefWatcher().watch(object);
+    private void showNewView() {
+        currentFragment = ZhiFuBaoIndexView.getInstance();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_view, currentFragment)
+                .commit();
     }
-  }
+
+    private ZhiFuBaoIndexView getCurrentFragment() {
+        return (ZhiFuBaoIndexView) getSupportFragmentManager().findFragmentById(R.id.content_view);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return currentFragment != null
+                && !currentFragment.isCollapseAnimRunning()
+                && super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        CleanLeakUtils.fixInputMethodManagerLeak(this);
+        checkItem(currentFragment);
+        checkItem(this);
+        currentFragment = null;
+    }
+
+    public void checkItem(Object object) {
+        if (object != null) {
+            Log.e("TAG", "checkItem: " + object);
+            ((MyApplication) getApplication()).getRefWatcher().watch(object);
+        }
+    }
 }
